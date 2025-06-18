dd871d23af892defdce24c73539f1d6b68d5a009
softCastella <agelin50@gmail.com>
2025-06-18 11:11:53 +0900

    03추가

---
+++ 03/modules/math.js
@@ -0,0 +1,16 @@
+function add(a,b) {
+    //더하기 함수
+    return a+b;
+}
+
+function subtract(a,b) {
+    //빼기 함수
+    return a-b;
+}
+
+module.exports = {
+    //외부로 내보내기
+    add,
+    subtract,
+}
+
